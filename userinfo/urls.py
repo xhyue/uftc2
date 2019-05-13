03930a740b4e2e870c407ccb6f0d392dd438b9be
tony <quhm@tedu.cn>
2019-05-13 10:16:17 +0800

    s

--- userinfo/urls.py
+++ userinfo/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
-
+from .views import *
 urlpatterns = [
+    url(r'^verifycode', VerificationCode.as_view(), name='verify_code'),
+    url(r'^cuslogin', CustomerLogin.as_view(), name='customer_login'),
+    url(r'^cusinfo', CustomerInfo.as_view(), name='customer_info'),
 
 ]
