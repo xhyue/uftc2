4a7966a8491b6d94248eabe5443ac4bd2ce84a3f
xhyue <xhyue_0209@sina.com>
2019-05-13 10:00:29 +0800

    login

--- userinfo/urls.py
+++ userinfo/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+from .views import *
 
 urlpatterns = [
+    url(r'verifycode', VerifyCodeView.as_view(), name='verify_code'),
+    url(r'phonelogin', PhoneLogin.as_view(), name='phone_login'),
 
 ]
